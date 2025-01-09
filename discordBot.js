7552648e5e4003b0051788da90b2df55dfcda70c
RebelPilotTyler <tylerwidenerlm@gmail.com>
2025-01-09 11:48:48 -0700

    Sending to two channels

--- discordBot.js
+++ discordBot.js
@@ -5,11 +5,12 @@ import { Client, GatewayIntentBits } from 'discord.js';
 // Environment variables: Set these in your Actions workflow or deployment settings
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const DISCORD_CHANNEL_ID = process.env.DISCORD_CHANNEL_ID;
+const DISCORD_CHANNEL_ID2 = process.env.DISCORD_CHANNEL_ID2;
 const WORLD_INDEX = process.env.WORLD_INDEX;
 const KEY = process.env.KEY;
 const NEW_VALUE = process.env.NEW_VALUE
 
-if (!BOT_TOKEN || !DISCORD_CHANNEL_ID) {
+if (!BOT_TOKEN || !DISCORD_CHANNEL_ID || !DISCORD_CHANNEL_ID2) {
     console.error("Error: BOT_TOKEN or CHANNEL_ID is not set.");
     process.exit(1);
 }
@@ -31,17 +32,27 @@ const controlSummary = Object.entries(control)
     .join(', ');
 
 
+let activeChannel = DISCORD_CHANNEL_ID;
+
 let MESSAGE_CONTENT = `World Index: ${WORLD_INDEX}\nKey: ${KEY}\nNew Value: ${NEW_VALUE}`; //WORKS!!!
 //let MESSAGE_CONTENT = `${world.name} data changed! ASTRAL Control at ${world.control.ASTRAL}%`
-if (KEY == 'control.ASTRAL') {
+/*if (KEY == 'control.ASTRAL') {
     MESSAGE_CONTENT = `${world.name} data changed! ASTRAL Control at ${world.control.ASTRAL}%`
+}*/
+if (KEY == 'control.ASTRAL') {
+    activeChannel = DISCORD_CHANNEL_ID;
+    MESSAGE_CONTENT = "Message to the test ASTRAL channel!";
+}
+if (KEY == 'control.SPIRIT') {
+    activeChannel = DISCORD_CHANNEL_ID2;
+    MESSAGE_CONTENT = "Message to the test SPIRIT channel!";
 }
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 
 client.once('ready', async () => {
     try {
-        const channel = await client.channels.fetch(DISCORD_CHANNEL_ID);
+        const channel = await client.channels.fetch(activeChannel);
         if (!channel?.isTextBased()) {
             console.error("Error: Channel is not valid or not a text-based channel.");
             process.exit(1);
