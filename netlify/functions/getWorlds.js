0d5d673332a1f5fa890698b8a95da3e94ac95c30
RebelPilotTyler <tylerwidenerlm@gmail.com>
2025-03-14 09:33:32 -0700

    Edited getWorlds filepath

--- netlify/functions/getWorlds.js
+++ netlify/functions/getWorlds.js
@@ -4,7 +4,7 @@ const path = require("path");
 exports.handler = async () => {
     try {
         // Read the file from Netlify's filesystem
-        const filePath = path.join(__dirname, "protected/worlds.json");
+        const filePath = path.join(__dirname, "../../protected/worlds.json");
         const worldsData = fs.readFileSync(filePath, "utf-8");
 
         return {
